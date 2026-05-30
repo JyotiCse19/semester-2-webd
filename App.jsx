b446c13490398d083b5d19f53ad62a9c6c4a081c
Jyoti yadav <jyotibkyadav6@gmail.com>
2026-05-30 16:48:17 +0530

    class

--- App.jsx
+++ App.jsx
@@ -1,37 +1,46 @@
 import { useState } from 'react' 
 import './App.css' 
- 
 function App() { 
- 
-  const [count, setCount] = useState(0) 
- 
-  const increment = () => { 
-    setCount(count + 1) 
+const [name, setName] = useState("") 
+const [email, setEmail] = useState("") 
+const [password, setPassword] = useState("") 
+function showData() { 
+if(name === "" || email === "" || password === ""){ 
+      alert("Please Fill All Fields") 
+    } 
+    else{ 
+      alert("Registered Successfully") 
+    } 
   } 
- 
-  const decrement = () => { 
-    setCount(count - 1) 
-  } 
- 
-  const reset = () => { 
-    setCount(0) 
-  } 
- 
-return (
+
+  return (
   <div className="container">
-    <h2>React Counter Application</h2>
+    <h2>Registration Form</h2>
 
-    <h2>{count}</h2>
+    <input
+      type="text"
+      placeholder="Enter Your Name"
+      onChange={(e) => setName(e.target.value)}
+    />
 
-    <button onClick={increment}>Increment</button>
-    <br /><br />
+    <input
+      type="email"
+      placeholder="Enter Your Email"
+      onChange={(e) => setEmail(e.target.value)}
+    />
 
-    <button onClick={decrement}>Decrement</button>
-    <br /><br />
+    <input
+      type="password"
+      placeholder="Enter Your Password"
+      onChange={(e) => setPassword(e.target.value)}
+    />
 
-    <button onClick={reset}>Reset</button>
+    <p>Name: {name}</p>
+    <p>Email: {email}</p>
+    <p>Password: {password}</p>
+
+    <button onClick={showData}>Submit</button>
   </div>
 )
 } 
- 
 export default App
